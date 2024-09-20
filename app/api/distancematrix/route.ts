96eb2b4bc300298e8c083b27ea3ad0f0c8083d61
Calebe-Xavier-Developer <57916053+Calebe-Xavier-Developer@users.noreply.github.com>
2024-09-20 13:32:49 -0300

    remove line

--- app/api/distancematrix/route.ts
+++ app/api/distancematrix/route.ts
@@ -5,7 +5,6 @@ export async function GET(request: NextRequest) {
     const origins = searchParams.get('origins');
     const destinations = searchParams.get('destinations');
     const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
-
     try {
         const response = await fetch(`https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origins}&destinations=${destinations}&key=${apiKey}`);
         const data = await response.json();
